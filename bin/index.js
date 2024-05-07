f1eb70b07b831f93d57c392a2f00a723680bbdbc
Redstone-1 <1825744594@qq.com>
2024-05-07 19:51:57 +0800

    feat: 将 bin 文件添加进 git

---
+++ bin/index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+/**
+ * 顶部的第一行是必须的
+ * @author xiwenge <1825744594@qq.com>
+ * @create 2023/06/25
+ */
+
+'use strict';
+
+const cli = require('../lib/index').default;
+
+cli.start();
